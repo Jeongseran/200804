3bf580ed86a3a572ee99cdef8b3b7b24c4484016
Jeongseran <yuanseran510@naver.com>
2020-08-05 16:06:48 +0900

    first commit

---
+++ ex02/print_food.py
@@ -0,0 +1,9 @@
+Python 3.7.7 (default, May  6 2020, 11:45:54) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("ice cream")
+ice cream
+>>> print("jelly")
+jelly
+>>> print("bread")
+bread
+>>> 
